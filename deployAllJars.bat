ca896a8bfecc45e1a019632fa0cd1aa5949d684f
Hacker6329 <Hacker6329@gmail.com>
2025-04-27 03:31:38 +0200

    Added multiple deployment system

---
+++ deployAllJars.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Deploying Linux Jar
+call ./gradlew.bat deployJarForLinux
+
+echo Deploying Windows Jar
+call ./gradlew.bat deployJarForWindows
+
+echo Deploying Mac Jar
+call ./gradlew.bat deployJarForMac
+
+echo Deploying Mac AArch64 (ARM) Jar
+call ./gradlew.bat deployJarForMacAArch64
+
+echo Deploying Complete!
