93d993da0cbef96267f36eb176154998b9257c82
Hacker6329 <Hacker6329@gmail.com>
2025-04-27 04:56:18 +0200

    Added check java version and check os

--- src/main/java/it/italiandudes/dnd_visualizer/DnD_Visualizer.java
+++ src/main/java/it/italiandudes/dnd_visualizer/DnD_Visualizer.java
@@ -4,13 +4,14 @@ import it.italiandudes.dnd_visualizer.db.DBManager;
 import it.italiandudes.dnd_visualizer.features.DiscordRichPresenceManager;
 import it.italiandudes.dnd_visualizer.javafx.Client;
 import it.italiandudes.dnd_visualizer.utils.Defs;
-import it.italiandudes.idl.common.InfoFlags;
-import it.italiandudes.idl.common.Logger;
+import it.italiandudes.idl.common.*;
+import org.apache.commons.lang3.SystemUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.jar.Attributes;
 
 public final class DnD_Visualizer {
 
@@ -68,12 +69,70 @@ public final class DnD_Visualizer {
         Runtime.getRuntime().addShutdownHook(new Thread(DiscordRichPresenceManager::shutdownRichPresence));
         Logger.log("Shutdown Hooks configured!", Defs.LOGGER_CONTEXT);
 
+        // Check Java Version
+        Logger.log("Verifying for Java 21...", Defs.LOGGER_CONTEXT);
+        if (!SystemUtils.IS_JAVA_21) {
+            Logger.log("The current java version is wrong, to run this jar you need Java 21!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
+            if (!isStartedFromLauncher()) {
+                Logger.close();
+                System.exit(-1);
+            }
+            return;
+        }
+        Logger.log("Java 21 Verified!", Defs.LOGGER_CONTEXT);
+
+        // Check OS
+        Logger.log("Verifying OS...", Defs.LOGGER_CONTEXT);
+        Logger.log("OS Name: " + SystemUtils.OS_NAME, Defs.LOGGER_CONTEXT);
+        Logger.log("OS Arch: " + SystemUtils.OS_ARCH, Defs.LOGGER_CONTEXT);
+        TargetPlatform currentPlatform = TargetPlatform.getCurrentPlatform();
+        Logger.log("Current OS Platform: " + (currentPlatform != null ? currentPlatform.getName() : "NOT RECOGNIZED"), Defs.LOGGER_CONTEXT);
+        if (currentPlatform == null) {
+            Logger.log("WARNING: Current OS Platform not recognized! An attempt to start the app will be done anyway.", new InfoFlags(true, false, false, true), Defs.LOGGER_CONTEXT);
+        }
+        try {
+            Attributes manifestAttributes = JarHandler.ManifestReader.readJarManifest(Defs.JAR_POSITION);
+            @Nullable String manifestTargetPlatform = JarHandler.ManifestReader.getValue(manifestAttributes, "Target-Platform");
+            if (manifestTargetPlatform == null) {
+                Logger.log("Target-Platform not specified in jar manifest, this jar shouldn't be used for release.", new InfoFlags(false, false, false, true), Defs.LOGGER_CONTEXT);
+            } else {
+                @Nullable TargetPlatform targetPlatform = TargetPlatform.fromManifestTargetPlatform(manifestTargetPlatform);
+                if (targetPlatform == null) {
+                    Logger.log("Target-Platform provided \"" + manifestTargetPlatform + "\" not recognized, this jar shouldn't be used for release.", new InfoFlags(false, false, false, true), Defs.LOGGER_CONTEXT);
+                } else {
+                    Logger.log("Jar Target-Platform: " + targetPlatform.getName(), Defs.LOGGER_CONTEXT);
+                    if (currentPlatform != null && !targetPlatform.isCurrentOS()) {
+                        Logger.log("Target-Platform \"" + targetPlatform.getName() + "\" incompatible with the current OS Platform!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
+                        if (!isStartedFromLauncher()) {
+                            Logger.close();
+                            System.exit(-1);
+                        }
+                        return;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Logger.log("An error has occurred while attempting to read jar manifest!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
+            if (!isStartedFromLauncher()) {
+                Logger.close();
+                System.exit(-1);
+            }
+            return;
+        }
+
         // Start the client
         try {
             Logger.log("Starting UI...", Defs.LOGGER_CONTEXT);
             Client.start(args);
         } catch (NoClassDefFoundError e) {
-            Logger.log("ERROR: TO RUN THIS JAR YOU NEED JAVA 21 WITH JAVAFX!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
+            Logger.log("ERROR: JAVAFX NOT FOUND!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
+            Logger.log(e, Defs.LOGGER_CONTEXT);
+            if (!isStartedFromLauncher()) {
+                Logger.close();
+                System.exit(-1);
+            }
+        } catch (Exception e) {
+            Logger.log("An exception has occurred while starting UI!", new InfoFlags(true, true, true, true), Defs.LOGGER_CONTEXT);
             Logger.log(e, Defs.LOGGER_CONTEXT);
             if (!isStartedFromLauncher()) {
                 Logger.close();
